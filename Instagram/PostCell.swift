b6296b7a979e9c51c53cda969802689ae82a94f3
Brandon Tran <brxntrxn@gmail.com>
2021-10-17 23:59:44 -0400

    Part 2

---
+++ Instagram/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  Instagram-Clone
+//
+//  Created by Brandon Tran on 10/10/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
